88ad28cb7c319ffa544a59b2c7b1bed990c34f52
よや <yoya@awm.jp>
2011-05-20 01:28:25 +0900

    glReadPixels の仕様変更に追随した

--- samples/yoya/screenshot.php
+++ samples/yoya/screenshot.php
@@ -5,24 +5,17 @@ function screenshot($output_file) {
     $height = glutGet(GLUT_WINDOW_HEIGHT);
     // OpenGL がレンダリングした画像pixel を吸い出す
     // red green blue alpha の順で並ぶ > GL_RGBA
-    
-    $pixels = array(); // 空配列を渡す
     glReadPixels(0, 0, $width, $height, GL_RGBA, GL_UNSIGNED_BYTE, $pixels);
 
     // GD でキャンバスを作る
     $im = imagecreatetruecolor($width, $height);
     $i = 0;
     // pixels 配列を R, G, B, A(無視) の順で解釈してイメージを作成
-    for ($y = $height ; $y >= 0; $y--) {
+    for ($y = $height -1 ; $y >= 0; $y--) {
     	for ($x = 0; $x < $width ; $x++) {
-            $rgb = $pixels[$i];
-            if ($rgb < 0) {
-                $rgb += 4294967296; // integer => float
-            }
-            $blue  = $rgb % 0x100 ; $rgb /= 0x100;
-            $green = $rgb % 0x100 ; $rgb /= 0x100;
-            $red   = $rgb % 0x100 ; $rgb /= 0x100;
-            $color = imagecolorallocate($im, $red, $green, $blue);
+            $p = $pixels[$i];
+            $alpha = 127 - $p['alpha'] / 2;
+            $color = imagecolorallocatealpha($im, $p['red'], $p['green'], $p['blue'], $alpha);
             imagesetpixel($im, $x, $y, $color); // pixel を埋めていく
             $i ++;
         }
