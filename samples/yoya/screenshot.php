f3821904ee860c18352c13a27d6b57297bc80e8d
よや <yoya@awm.jp>
2011-07-12 01:47:49 +0900

    glReadPixels の戻り値を変えたので、合わせて変更

--- samples/yoya/screenshot.php
+++ samples/yoya/screenshot.php
@@ -6,18 +6,16 @@ function screenshot($output_file) {
     // OpenGL がレンダリングした画像pixel を吸い出す
     // red green blue alpha の順で並ぶ > GL_RGBA
     glReadPixels(0, 0, $width, $height, GL_RGBA, GL_UNSIGNED_BYTE, $pixels);
-
     // GD でキャンバスを作る
     $im = imagecreatetruecolor($width, $height);
     $i = 0;
     // pixels 配列を R, G, B, A(無視) の順で解釈してイメージを作成
     for ($y = $height -1 ; $y >= 0; $y--) {
     	for ($x = 0; $x < $width ; $x++) {
-            $p = $pixels[$i];
-            $alpha = 127 - $p['alpha'] / 2;
-            $color = imagecolorallocatealpha($im, $p['red'], $p['green'], $p['blue'], $alpha);
+            $alpha = 127 - $pixels[$i+3] / 2;
+            $color = imagecolorallocatealpha($im, $pixels[$i], $pixels[$i+1], $pixels[$i+2], $alpha);
             imagesetpixel($im, $x, $y, $color); // pixel を埋めていく
-            $i ++;
+            $i += 4;
         }
     }
     imagepng($im, $output_file); // PNG ファイルとして保存
